4dc5da6b8c5567a8cdf0b1dfe3dfaff9f8c0d6de
ANURAGYADAV02 <py9151223314@gmail.com>
2025-02-05 23:58:23 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import Background from "./componants/Background"
+import Foreground from "./componants/Foreground"
+
+const App = () => {
+
+  return (
+    <div className=" relative w-full h-screen bg-zinc-800">
+     <Background/>
+     <Foreground/>
+    </div>
+  )
+}
+
+export default App
